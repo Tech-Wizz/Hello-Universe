7eb5db7af4ece1313b2825654184fa359b32cfb4
Kruize Derek Christensen <christensen.kruize@gmail.com>
2023-12-16 22:26:30 -0700

    Fixed to display on iphone

--- swift/HelloWorld.swift
+++ swift/HelloWorld.swift
@@ -1,1 +1,25 @@
-print("Hello, World!")
+//
+//  HelloWorld.swift
+//  HelloWorld
+//
+//  Created by Kruize Christensen on 12/16/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack{
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
